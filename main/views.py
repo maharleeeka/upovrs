1ec907330db8b563292b1eb7b460aefa563c1be4
maharleeeka <maharlikaalberca@gmail.com>
2016-10-22 00:06:35 -0800

    add success page successful routing after form submit

--- main/views.py
+++ main/views.py
@@ -4,14 +4,12 @@ from django.views.generic import TemplateView, CreateView
 from django.views import View
 from main.models import Venue, Equipment, Request
 from main import forms
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.edit import ModelFormMixin
 
 
 def index(request):
     return HttpResponse("request")
-
-def requestform(request):
-	template = 'main/request_form.html'
-	return render(request,template)
 
 def guidelines(request):
 	template = 'main/guidelines.html'
@@ -19,6 +17,10 @@ def guidelines(request):
 
 def login(request):
 	template = 'main/login.html'
+	return render(request,template)
+
+def success(request):
+	template = 'main/success.html'
 	return render(request,template)
 
 class home(TemplateView):
@@ -35,7 +37,7 @@ class RequestView(CreateView):
  		return super(ModelFormMixin, self).form_valid(form)
 
 	def get_success_url(self):
-		return reverse('main:requestform')
+		return reverse_lazy('success')
 
 	def get_context_data(self, **kwargs):
 		context = super(RequestView, self).get_context_data(**kwargs)
