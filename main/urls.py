50816a03f1429d2b9c05c011fa94908c056e4813
maharleeeka <maharlikaalberca@gmail.com>
2016-10-22 21:14:27 -0800

    resolve url

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.MainView.as_view(), name='index'),
+    # url(r'^$', views.MainView.as_view(), name='index'),
     # url(r'^$', views.HomepageView.as_view(), name='index'),
     url(r'^requestform/$', views.RequestView.as_view(), name='requestform'),
     url(r'^guidelines/$', views.GuidelineView.as_view(), name='guidelines'),
