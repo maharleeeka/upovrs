671130517338703af11db8e1b70c40dd1511ed8f
Charie Mae <maechan014@gmail.com>
2016-10-24 13:57:39 +0800

    js for add date

--- main/static/js/requestform.js
+++ main/static/js/requestform.js
@@ -1,16 +1,67 @@
-//var i = 0;
-//var original = document.getElementById('schedule');
-
 function repeat() {
-  // var clone = original.cloneNode(true);
-  // clone.id = "schedule" + ++i; 
-  // original.parentNode.appendChild(clone);
-
   var div = document.getElementById('schedule'),
     clone = div.cloneNode(true); // true means clone all childNodes and all event handlers
 	clone.id = "schedule1";
 	document.body.appendChild(clone);
 }
+
+
+function dateAdded() {
+  /* by charie: PLEASE DO NOT MODIFY THIS FUNCTION */
+
+  /*get the input values*/
+  var date = document.getElementsByName('date_needed')[0].value;
+  var timeFrom = document.getElementsByName('time_from')[0].value;
+  var timeTo = document.getElementsByName('time_to')[0].value;
+
+  /* create new elements on html*/
+  var iDiv = document.createElement('div');
+  var input_1 = document.createElement('input');
+  var input_2 = document.createElement('input');
+  var input_3 = document.createElement('input');
+  var btn = document.createElement ('button');
+  
+  /* set id and class for div*/
+  iDiv.id = 'added_date';
+  iDiv.className = 'col-md-12 block';
+
+  /* create name attribute for the input tags*/
+  var att_1 = document.createAttribute('name');
+  var att_2 = document.createAttribute('name');
+  var att_3 = document.createAttribute('name');
+
+  /*set the inputs*/
+  input_1.setAttributeNode(att_1);
+  att_2.value = "date_needed";
+  input_1.className = 'col-md-3';
+  input_1.value = date;
+ 
+  input_2.setAttributeNode(att_2);
+  att_2.value = "time_from";
+  input_2.className = 'col-md-3';
+  input_2.value = timeFrom;
+
+  input_3.setAttributeNode(att_3);
+  att_3.value = "time_to";
+  input_3.className = 'col-md-3';
+  input_3.value = timeTo;  
+
+  /* append input tags to div tag*/
+  iDiv.appendChild(input_1);
+  iDiv.appendChild(input_2);
+  iDiv.appendChild(input_3);
+
+  /* append div tag to html body*/
+   document.getElementById('schedule').appendChild(iDiv);
+
+}
+
+
+function requireDateField(){
+  /* by charie: PLEASE DO NOT MODIFY THIS FUNCTION */
+}
+
+
 function validateForm(){
 	console.log('ja');
 	var valid = true;
@@ -32,6 +83,7 @@ function validateForm(){
   	}
   }
 }
+
 
 function checkDate() {
     var EnteredDate = document.getElementById("date_needed").value; //for javascript
