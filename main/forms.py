163d06d1a495175aa92f8c4f290ad07ca795d105
jessduran <duran.jessahmarie@gmail.com>
2016-10-26 20:55:31 +0800

    def post method is now in RequestView

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 from django.forms import ModelForm
-from main.models import Request
+from main.models import Request, RentedEquipment
 
 
 class RequestForm(forms.ModelForm):
 	class Meta:
 		model = Request
-		fields = '__all__'
+		fields = ['name', 'organization', 'purpose', 'participants', 'speakers', 'venue_id', 'date_needed', 'time_from', 'time_to', 'status']
+
+class RentedEqForm(forms.ModelForm):
+	class Meta:
+		model = RentedEquipment
+		exclude = ('request',)
+
+
--- main/models.py
+++ main/models.py
@@ -46,14 +46,13 @@ class Request(models.Model):
 	time_from = models.TimeField(blank=True, null=True)
 	time_to = models.TimeField(blank=True, null=True)
 	
-	
-
 	def __str__(self):
 		return str(self.name)
 
 class RentedEquipment(models.Model):
 	request_id = models.ForeignKey(Request, models.DO_NOTHING, db_column='request_id')
-	equipment_id = models.ForeignKey(Equipment, models.DO_NOTHING, db_column='equipment_id')
+	#equipment_id = models.ForeignKey(Equipment, models.DO_NOTHING, db_column='equipment_id', null=True, blank=True)
+	equipment_id = models.DecimalField(max_digits = 4, decimal_places=0, null=True)
 	unit = models.DecimalField(max_digits = 4, decimal_places=0, null=True)
 
 	def __str__(self):
