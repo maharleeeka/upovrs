7ca13c47dafc3340bd1ddfc567578589b07ab8a6
Faye Lomente <faye@newlogic.io>
2019-10-25 18:39:22 +0800

    Fix the login and logout mechanism of the web app
    Login changes:
    	- Add security to the login by using the default Django authentication.
    	- Used CSRF token
    	- Show errors in the login page
    	- Required authentication across all views
    Logout changes:
    	- Add logout mechanism
    	- Redirect logout to login page

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.MainView.as_view(), name='index'),
+    url(r'^$', views.UserLoginView.as_view(), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^index/$', views.MainView.as_view(), name='index'),
     url(r'^requestform/$', views.RequestView.as_view(), name='requestform'),
     url(r'^guidelines/$', views.GuidelineView.as_view(), name='guidelines'),
     url(r'^rates/$', views.RateView.as_view(), name='rate'),
     url(r'^success/$', views.SuccessView.as_view(), name='success'),
-    url(r'^/$', views.LoginView.as_view(), name='login'),
 ]
